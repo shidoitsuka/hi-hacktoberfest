a6dee109099eede5a52606bd55eb535219c9d138
Salman <61409924+SlmnMEDIA@users.noreply.github.com>
2020-10-03 10:50:03 +0700

    Add files via upload

---
+++ code/java/main.java
@@ -0,0 +1,24 @@
+//contoh program hitung luas lingkaran
+public class Main {
+	public static void main(String[] args) {
+	  
+//deklarasi variabel untuk jari jari dan luas
+	  double jarijari;
+	  double luas;
+	  
+	  jarijari = 20;
+	  
+//menentukan rumus hitung luas lingkaran
+     luas = 3.14*jarijari*jarijari;
+     
+//tampilkan hasilnya
+//tampilkan kalimatnya
+     System.out.print("Luas lingkaran dengan jari jari = ");
+//mengambil nilai jari jari dari variabel
+     System.out.println(jarijari);
+//tampil string dan mengambil nilai luas dari variabel
+     System.out.println("adalah = " + luas);
+	  
+		
+	}
+}
